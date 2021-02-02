0a757d042b8c2f4e349827c75c6df8cdba2619d3
iarivas <iarivas@miners.utep.edu>
2021-02-02 14:11:54 -0700

    Tokenizer.h first few methods.

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -5,26 +5,57 @@
 /* Return true (non-zero) if c is a whitespace characer
    (' ', '\t', or '\n').
    Zero terminators are not printable (therefore false) */
-int space_char(char c);
+int space_char(char c){
+  if(c == ' ' || c == '\t' || c == '\n'){
+    return 1;
+  }return 0;
+}
 
 /* Return true (non-zero) if c is a non-whitespace
    character (not space, tab, or newline).
    Zero terminators are not printable (therefore false) */
-int non_space_char(char c);
+int non_space_char(char c){
+  if(c != ' ' || c != '\t' || c != '\n'){
+    return 1;
+  }return 0;
+}
 
 /* Returns a pointer to the first character of the next
    space-separated word in zero-terminated str.  Return a zero pointer if
    str does not contain any words.
    str is assumed to be pointing to a space character */
-char *word_start(char *str);
+char *word_start(char *str){
+  if(str[0] == '\0') { return '\0'; }
+  int i = 0;
+  while(space_char(str[i])){
+    i++;
+  }
+  return &str[i];
+}
 
 /* Returns a pointer terminator to the first space character or string-terminator
    following str in a zero terminated string.
    str is assumed to be pointing to a non-space character*/
-char *word_end(char *str);
+char *word_end(char *str){
+  if(str[0] == '\0') { return '\0'; }
+  int i = 0;
+  while(non_space_char(str[i])){
+    i++;
+  }
+  return &str[i];
+}
 
 /* Counts the number of space seperated words in the string argument. */
-int count_words(char *str);
+int count_words(char *str){
+  int count = 0;
+  char *p = word_start(str[0]);
+  if(p[0] == '\0'){ return 0; }
+  while(p[i] != '\0'){
+    count++;
+    p = word_end(p[0]);
+    p = word_start(p[0]);
+  }
+}
 
 /* Returns a freshly allocated new zero-terminated string
    containing <len> chars from <inStr> */
