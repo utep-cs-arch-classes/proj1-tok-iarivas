09ef370ddaf33848a0c0d0e52dfa3d9818e293fc
iarivas <iarivas@miners.utep.edu>
2021-02-09 12:30:05 -0700

    Main collects user input

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include "tokenizer.h"
 #include <stdio.h>
+#define LIMIT 100
+char arr[LIMIT];
+
 
 int main(){
-  int c = space_char(' ');
-  printf("%d\n", c);
+  int i = 0;
+  for(char input_char; ((input_char = getchar()) != "\n") && (i < LIMIT); i++){
+    arr[i] = input_char;
+    putchar(input_char);
+  }
+  printf("\n");
+
+  for(int j = 0; j < i; j++){
+    if(space_char(arr[j])){ printf("Space Char \n"); }
+    else if(non_space_char(arr[j])){ printf("Non Space Char \n"); }
+    else{ printf("Error \n"); }
+  }
+  return 1;
 }
+
+
