aa3688fe1248404e8912c7885a58df01eedbe99d
iarivas <iarivas@miners.utep.edu>
2021-02-24 21:02:00 -0700

    Recloned Reposiroty, Main.c Update, and Tokenizer.c update

--- src/main.c
+++ src/main.c
@@ -1,23 +1,44 @@
 #include "tokenizer.h"
 #include <stdio.h>
+#include <stdlib.h>
 #define LIMIT 100
 char arr[LIMIT];
 
 
-int main(){
-  int i = 0;
-  for(char input_char; ((input_char = getchar()) != '\n') && (i < LIMIT); i++){
-    arr[i] = input_char;
-    putchar(input_char);
+int main()
+{
+  char input[LIMIT];
+  goto Options;
+ Options:
+  printf("Select an option below.\n");
+  printf("Input 'i' to input a sentence.\n");
+  printf("Input 'h' to view history.\n");
+  printf("Input '!' and a number to get a specific token from history.\n");
+  printf("Input 'q' to exit program.\n");
+  printf(">");
+  fgets(input, LIMIT, stdin);
+  if(input[0] == 'i'){ goto input; }
+  else if(input[0] == 'h'){ goto History; }
+  else if(input[0] == '!'){ goto gHistory; }
+  else if(input[0] == 'q'){ goto quit; }
+  else{
+    printf("Error please try again.\n");
+    goto Options;
   }
-  printf("\n");
-
-  for(int j = 0; j < i; j++){
-    if(space_char(arr[j])){ printf("Space Char \n"); }
-    else if(non_space_char(arr[j])){ printf("Non Space Char \n"); }
-    else{ printf("Error \n"); }
-  }
-  return 1;
+ input:
+  printf("Input sentence below. \n>");
+  fgets(input, LIMIT, stdin);
+  char **tokens = tokenize(input);
+  print_tokens(tokens);
+  free_tokens(tokens);
+  goto Options;
+ History:
+  printf("History.\n");
+  goto Options;
+ gHistory:
+  printf("gHistory.\n");
+  goto Options;
+ quit:
+  printf("Thank you. Goodbye.\n");
+  return 0;
 }
-
-
--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,24 +1,23 @@
 #include "tokenizer.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-int main2(){
-  printf("start\n");
-}
-
-int space_char(char c){
-  if(c == ' ' || c == '\t' || c == '\n' ){
+int space_char(char c)
+{
+  if((c == ' ' || c == '\t') && c != 0){
     return 1;
   }return 0;
 }
 
-int non_space_char(char c){
-  if(c != ' ' || c != '\t' || c != '\n'){
+int non_space_char(char c)
+{
+  if((c != ' ' || c != '\t') && c != 0){
     return 1;
   }return 0;
 }
 
-char *word_start(char *str){
-  if(str[0] == '\0') { return 0; }
+char *word_start(char *str)
+{
   int i = 0;
   while(space_char(str[i])){
     i++;
@@ -26,35 +25,77 @@ char *word_start(char *str){
   return &str[i];
 }
 
-char *word_end(char *str){
-  if(str[0] == '\0') { return 0;}
-  int i =0;
-  while(non_space_char(str[0])){
+char *word_end(char *str)
+{
+  int i = 0;
+  while(non_space_char(str[i])){
     i++;
   }
   return &str[i];
 }
 
-int count_words(char *str){
+int count_words(char *str)
+{
   int count = 0;
-  char *p = word_start(&str[0]);
-  if(p[0] == '\0') { return 0;}
-  while(p[0] != '\0'){
-    p = word_end(&p[0]);
-    p = word_start(&p[0]);
+  char *ptr = str;
+  while(*ptr != '\0')
+  {
+    ptr = word_start(ptr);
+    ptr = word_end(ptr);
     count++;
   }
   return count;
 }
 
-/*
+char *copy_str(char *src, short len)
+{
+  char *copy = malloc((len+1) * sizeof(char));
+  int i;
+  for(i = 0; i < len; i++){
+    copy[i] = src[i];
+  }
+  copy[i] = 0;
+  return copy;
+}
 
-char *copy_str(char *instr, short len){}
+char **tokenize(char* str)
+{
+  int words = count_words(str);
+  char **tokens = malloc((words+1) * sizeof(char *));
+  int i;
+  char *ptr = str;
+  for(i = 0; i < words; i++){
+    char *start = word_start(ptr);
+    char *end = word_end(ptr);
+    char *ptr = start;
+    int len = 0;
+    while(ptr != end){
+      ptr++;
+      len++;
+    }
+    tokens[i] = copy_str(start, len);
+    ptr = end;
+  }
+  tokens[i] = 0;
+  return tokens;
+}
 
-char **tokenize(char* str){}
+void print_tokens(char **tokens)
+{
+  int i = 0;
+  while(tokens[i] != 0){
+    printf("%s\n", tokens[i]);
+    i++;
+  }
+}
 
-void print_tokens(char **tokens){}
-
-void free_tokens(char **tokens){}
-
-*/
+void free_tokens(char **tokens)
+{
+  int i = 0;
+  while(tokens[i] != 0){
+    free(tokens[i]);
+    i++;
+  }
+  free(tokens);
+}
+ 
